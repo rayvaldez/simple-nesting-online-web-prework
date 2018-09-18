6e66ab29ca6987de03816b8c35b979b3db4b87b9
Ray Valdez <ray_valdez@hotmail.co.uk>
2018-09-18 18:12:20 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -65,13 +65,6 @@ def adding_matz
 #   :known_for => "Ruby",
 #    :languages => ["LISP", "C"]
 # }
-	programmer_hash[:yukihiro_matsumoto] =
-	{
-	:known_for => "Ruby",
-	:languages => ["LISP", "C"]
-	}
-
-
 	programmer_hash =
  		{
         :grace_hopper => {
@@ -87,7 +80,11 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
+	programmer_hash[:yukihiro_matsumoto] =
+	 	{
+	 	:known_for => "Ruby",
+	 	:languages => ["LISP", "C"]
+	 	}
 
 end
 
